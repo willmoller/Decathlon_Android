eba21f1ae38d132842115ed79f6dd92f45c546bc
Will Moller <will.moller@gmail.com>
2021-03-28 12:06:53 -0500

    Finished 400 Metres Activity!

--- app/src/main/java/com/example/encore/Event100Metres.java
+++ app/src/main/java/com/example/encore/Event100Metres.java
@@ -11,16 +11,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Random;
 
 public class Event100Metres extends AppCompatActivity {
 
     private static final Random RANDOM = new Random();
     private Dice rolledDice, bankedDiceSet1, bankedDiceSet2;
-    private ArrayList<ImageView> ivRolledDice, ivBankedDiceSet1, ivBankedDiceSet2;
-    private ArrayList<String> rolledKeys, bankKeys1, bankKeys2;
     private int bankedDice, totalScore;
-    private Button rollDice, keepDice, resetGame, leaveGame;
+    private Button rollDice;
+    private Button keepDice;
+    private Button resetGame;
 
     private TextView rollsLeftText, totalScoreText;
     private int rollsLeft = 7;
@@ -35,19 +36,19 @@ public class Event100Metres extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event100_metres);
 
-        rollDice = (Button) findViewById(R.id.button_100M_roll);
-        keepDice = (Button) findViewById(R.id.button_100M_keep);
-        resetGame = (Button) findViewById(R.id.button_100M_replay);
+        rollDice = findViewById(R.id.button_100M_roll);
+        keepDice = findViewById(R.id.button_100M_keep);
+        resetGame = findViewById(R.id.button_100M_replay);
         resetGame.setEnabled(false);
-        leaveGame = (Button) findViewById(R.id.button_100M_done);
+        Button leaveGame = findViewById(R.id.button_100M_done);
 
-        rollsLeftText = (TextView) findViewById(R.id.tv100MRollsRemain);
-        totalScoreText = (TextView) findViewById(R.id.tv100MScore);
+        rollsLeftText = findViewById(R.id.tv100MRollsRemain);
+        totalScoreText = findViewById(R.id.tv100MScore);
         bankedDice = 0;
         totalScore = 0;
 
-        ivRolledDice = new ArrayList<ImageView>();
-        rolledKeys = new ArrayList<String>();
+        ArrayList<ImageView> ivRolledDice = new ArrayList<>();
+        ArrayList<String> rolledKeys = new ArrayList<>();
 
         rolledKeys.add("rollDie1");
         ivRolledDice.add(findViewById(R.id.iv100MrollDie1));
@@ -61,10 +62,10 @@ public class Event100Metres extends AppCompatActivity {
         rolledKeys.add("rollDie4");
         ivRolledDice.add(findViewById(R.id.iv100MrollDie4));
 
-        ivBankedDiceSet1 = new ArrayList<ImageView>();
-        ivBankedDiceSet2 = new ArrayList<ImageView>();
-        bankKeys1 = new ArrayList<String>();
-        bankKeys2 = new ArrayList<String>();
+        ArrayList<ImageView> ivBankedDiceSet1 = new ArrayList<>();
+        ArrayList<ImageView> ivBankedDiceSet2 = new ArrayList<>();
+        ArrayList<String> bankKeys1 = new ArrayList<>();
+        ArrayList<String> bankKeys2 = new ArrayList<>();
 
         bankKeys1.add("bankDie1");
         bankKeys1.add("bankDie2");
@@ -92,94 +93,91 @@ public class Event100Metres extends AppCompatActivity {
 
         mp = MediaPlayer.create(Event100Metres.this, R.raw.dice);
 
-        rollDice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    if (mp.isPlaying()) {
-                        mp.stop();
-                        mp.release();
-                        mp = MediaPlayer.create(Event100Metres.this, R.raw.dice);
-                    } mp.start();
-                } catch(Exception e) { e.printStackTrace(); }
+        rollDice.setOnClickListener(v -> {
+            try {
+                if (mp.isPlaying()) {
+                    mp.stop();
+                    mp.release();
+                    mp = MediaPlayer.create(Event100Metres.this, R.raw.dice);
+                } mp.start();
+            } catch(Exception e) { e.printStackTrace(); }
 
-                final Animation anim1 = AnimationUtils.loadAnimation(Event100Metres.this, R.anim.shake);
-                final Animation anim2 = AnimationUtils.loadAnimation(Event100Metres.this, R.anim.shake);
-                final Animation anim3 = AnimationUtils.loadAnimation(Event100Metres.this, R.anim.shake);
-                final Animation anim4 = AnimationUtils.loadAnimation(Event100Metres.this, R.anim.shake);
+            final Animation anim1 = AnimationUtils.loadAnimation(Event100Metres.this, R.anim.shake);
+            final Animation anim2 = AnimationUtils.loadAnimation(Event100Metres.this, R.anim.shake);
+            final Animation anim3 = AnimationUtils.loadAnimation(Event100Metres.this, R.anim.shake);
+            final Animation anim4 = AnimationUtils.loadAnimation(Event100Metres.this, R.anim.shake);
 
-                keepDice.setEnabled(true);
+            keepDice.setEnabled(true);
 
-                rollsLeft--;
+            rollsLeft--;
 
-                if (rollsLeft == 1 && bankedDice == 0){
-                    rollDice.setEnabled(false);
-                } else if (rollsLeft == 0 && bankedDice == 4){
-                    rollDice.setEnabled(false);
+            if (rollsLeft == 1 && bankedDice == 0){
+                rollDice.setEnabled(false);
+            } else if (rollsLeft == 0 && bankedDice == 4){
+                rollDice.setEnabled(false);
+            }
+
+            final Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
                 }
 
-                final Animation.AnimationListener animationListener = new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    int value = randomDiceValue();
+                    int res = 0;
+                    switch (value) {
+                        case 1:
+                            res = R.drawable.die1;
+                            break;
+                        case 2:
+                            res = R.drawable.die2;
+                            break;
+                        case 3:
+                            res = R.drawable.die3;
+                            break;
+                        case 4:
+                            res = R.drawable.die4;
+                            break;
+                        case 5:
+                            res = R.drawable.die5;
+                            break;
+                        case 6:
+                            res = R.drawable.die6;
+                            break;
                     }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        int value = randomDiceValue();
-                        int res = 0;
-                        switch (value) {
-                            case 1:
-                                res = R.drawable.die1;
-                                break;
-                            case 2:
-                                res = R.drawable.die2;
-                                break;
-                            case 3:
-                                res = R.drawable.die3;
-                                break;
-                            case 4:
-                                res = R.drawable.die4;
-                                break;
-                            case 5:
-                                res = R.drawable.die5;
-                                break;
-                            case 6:
-                                res = R.drawable.die6;
-                                break;
-                        }
-                        if (animation == anim1){
-                            rolledDice.getDiceList().get("rollDie1").getDieFaceView().setImageResource(res);
-                            rolledDice.getDiceList().get("rollDie1").setValue(value);
-                        }else if (animation == anim2){
-                            rolledDice.getDiceList().get("rollDie2").getDieFaceView().setImageResource(res);
-                            rolledDice.getDiceList().get("rollDie2").setValue(value);
-                        }else if (animation == anim3){
-                            rolledDice.getDiceList().get("rollDie3").getDieFaceView().setImageResource(res);
-                            rolledDice.getDiceList().get("rollDie3").setValue(value);
-                        } else if (animation == anim4){
-                            rolledDice.getDiceList().get("rollDie4").getDieFaceView().setImageResource(res);
-                            rolledDice.getDiceList().get("rollDie4").setValue(value);
-                        }
+                    if (animation == anim1){
+                        Objects.requireNonNull(rolledDice.getDiceList().get("rollDie1")).getDieFaceView().setImageResource(res);
+                        Objects.requireNonNull(rolledDice.getDiceList().get("rollDie1")).setValue(value);
+                    }else if (animation == anim2){
+                        Objects.requireNonNull(rolledDice.getDiceList().get("rollDie2")).getDieFaceView().setImageResource(res);
+                        Objects.requireNonNull(rolledDice.getDiceList().get("rollDie2")).setValue(value);
+                    }else if (animation == anim3){
+                        Objects.requireNonNull(rolledDice.getDiceList().get("rollDie3")).getDieFaceView().setImageResource(res);
+                        Objects.requireNonNull(rolledDice.getDiceList().get("rollDie3")).setValue(value);
+                    } else if (animation == anim4){
+                        Objects.requireNonNull(rolledDice.getDiceList().get("rollDie4")).getDieFaceView().setImageResource(res);
+                        Objects.requireNonNull(rolledDice.getDiceList().get("rollDie4")).setValue(value);
                     }
+                }
 
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
+                @Override
+                public void onAnimationRepeat(Animation animation) {
 
-                    }
-                };
-                anim1.setAnimationListener(animationListener);
-                anim2.setAnimationListener(animationListener);
-                anim3.setAnimationListener(animationListener);
-                anim4.setAnimationListener(animationListener);
+                }
+            };
+            anim1.setAnimationListener(animationListener);
+            anim2.setAnimationListener(animationListener);
+            anim3.setAnimationListener(animationListener);
+            anim4.setAnimationListener(animationListener);
 
-                rolledDice.getDiceList().get("rollDie1").getDieFaceView().startAnimation(anim1);
-                rolledDice.getDiceList().get("rollDie2").getDieFaceView().startAnimation(anim2);
-                rolledDice.getDiceList().get("rollDie3").getDieFaceView().startAnimation(anim3);
-                rolledDice.getDiceList().get("rollDie4").getDieFaceView().startAnimation(anim4);
+            Objects.requireNonNull(rolledDice.getDiceList().get("rollDie1")).getDieFaceView().startAnimation(anim1);
+            Objects.requireNonNull(rolledDice.getDiceList().get("rollDie2")).getDieFaceView().startAnimation(anim2);
+            Objects.requireNonNull(rolledDice.getDiceList().get("rollDie3")).getDieFaceView().startAnimation(anim3);
+            Objects.requireNonNull(rolledDice.getDiceList().get("rollDie4")).getDieFaceView().startAnimation(anim4);
 
-                rollsLeftText.setText(Integer.toString(rollsLeft));
-            }
+            rollsLeftText.setText(Integer.toString(rollsLeft));
         });
 
         keepDice.setOnClickListener(new View.OnClickListener() {
@@ -232,26 +230,26 @@ public class Event100Metres extends AppCompatActivity {
     private void bankRolledDice(){
         // move rollDie's to bankDie's
         if (bankedDice == 0){
-            bankedDiceSet1.getDiceList().get("bankDie1").setValue(rolledDice.getDiceList().get("rollDie1").getValue());
-            bankedDiceSet1.getDiceList().get("bankDie1").getDieFaceView().setImageDrawable(rolledDice.getDiceList().get("rollDie1").getDieFaceView().getDrawable());
-            bankedDiceSet1.getDiceList().get("bankDie2").setValue(rolledDice.getDiceList().get("rollDie2").getValue());
-            bankedDiceSet1.getDiceList().get("bankDie2").getDieFaceView().setImageDrawable(rolledDice.getDiceList().get("rollDie2").getDieFaceView().getDrawable());
-            bankedDiceSet1.getDiceList().get("bankDie3").setValue(rolledDice.getDiceList().get("rollDie3").getValue());
-            bankedDiceSet1.getDiceList().get("bankDie3").getDieFaceView().setImageDrawable(rolledDice.getDiceList().get("rollDie3").getDieFaceView().getDrawable());
-            bankedDiceSet1.getDiceList().get("bankDie4").setValue(rolledDice.getDiceList().get("rollDie4").getValue());
-            bankedDiceSet1.getDiceList().get("bankDie4").getDieFaceView().setImageDrawable(rolledDice.getDiceList().get("rollDie4").getDieFaceView().getDrawable());
+            Objects.requireNonNull(bankedDiceSet1.getDiceList().get("bankDie1")).setValue(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie1")).getValue());
+            Objects.requireNonNull(bankedDiceSet1.getDiceList().get("bankDie1")).getDieFaceView().setImageDrawable(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie1")).getDieFaceView().getDrawable());
+            Objects.requireNonNull(bankedDiceSet1.getDiceList().get("bankDie2")).setValue(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie2")).getValue());
+            Objects.requireNonNull(bankedDiceSet1.getDiceList().get("bankDie2")).getDieFaceView().setImageDrawable(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie2")).getDieFaceView().getDrawable());
+            Objects.requireNonNull(bankedDiceSet1.getDiceList().get("bankDie3")).setValue(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie3")).getValue());
+            Objects.requireNonNull(bankedDiceSet1.getDiceList().get("bankDie3")).getDieFaceView().setImageDrawable(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie3")).getDieFaceView().getDrawable());
+            Objects.requireNonNull(bankedDiceSet1.getDiceList().get("bankDie4")).setValue(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie4")).getValue());
+            Objects.requireNonNull(bankedDiceSet1.getDiceList().get("bankDie4")).getDieFaceView().setImageDrawable(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie4")).getDieFaceView().getDrawable());
 
             totalScore += bankedDiceSet1.ScoreDice();
             bankedDiceSet1.MakeVisible();
         } else {
-            bankedDiceSet2.getDiceList().get("bankDie5").setValue(rolledDice.getDiceList().get("rollDie1").getValue());
-            bankedDiceSet2.getDiceList().get("bankDie5").getDieFaceView().setImageDrawable(rolledDice.getDiceList().get("rollDie1").getDieFaceView().getDrawable());
-            bankedDiceSet2.getDiceList().get("bankDie6").setValue(rolledDice.getDiceList().get("rollDie2").getValue());
-            bankedDiceSet2.getDiceList().get("bankDie6").getDieFaceView().setImageDrawable(rolledDice.getDiceList().get("rollDie2").getDieFaceView().getDrawable());
-            bankedDiceSet2.getDiceList().get("bankDie7").setValue(rolledDice.getDiceList().get("rollDie3").getValue());
-            bankedDiceSet2.getDiceList().get("bankDie7").getDieFaceView().setImageDrawable(rolledDice.getDiceList().get("rollDie3").getDieFaceView().getDrawable());
-            bankedDiceSet2.getDiceList().get("bankDie8").setValue(rolledDice.getDiceList().get("rollDie4").getValue());
-            bankedDiceSet2.getDiceList().get("bankDie8").getDieFaceView().setImageDrawable(rolledDice.getDiceList().get("rollDie4").getDieFaceView().getDrawable());
+            Objects.requireNonNull(bankedDiceSet2.getDiceList().get("bankDie5")).setValue(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie1")).getValue());
+            Objects.requireNonNull(bankedDiceSet2.getDiceList().get("bankDie5")).getDieFaceView().setImageDrawable(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie1")).getDieFaceView().getDrawable());
+            Objects.requireNonNull(bankedDiceSet2.getDiceList().get("bankDie6")).setValue(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie2")).getValue());
+            Objects.requireNonNull(bankedDiceSet2.getDiceList().get("bankDie6")).getDieFaceView().setImageDrawable(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie2")).getDieFaceView().getDrawable());
+            Objects.requireNonNull(bankedDiceSet2.getDiceList().get("bankDie7")).setValue(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie3")).getValue());
+            Objects.requireNonNull(bankedDiceSet2.getDiceList().get("bankDie7")).getDieFaceView().setImageDrawable(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie3")).getDieFaceView().getDrawable());
+            Objects.requireNonNull(bankedDiceSet2.getDiceList().get("bankDie8")).setValue(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie4")).getValue());
+            Objects.requireNonNull(bankedDiceSet2.getDiceList().get("bankDie8")).getDieFaceView().setImageDrawable(Objects.requireNonNull(rolledDice.getDiceList().get("rollDie4")).getDieFaceView().getDrawable());
 
             totalScore += bankedDiceSet2.ScoreDice();
             bankedDiceSet2.MakeVisible();
--- app/src/main/java/com/example/encore/Event400Metres.java
+++ app/src/main/java/com/example/encore/Event400Metres.java
@@ -2,15 +2,223 @@ package com.example.encore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Event400Metres extends AppCompatActivity {
+
+    private static final Random RANDOM = new Random();
+    private Dice rolledDice;
+    private ArrayList<ImageView> ivRolledDice;
+    private ArrayList<String> rolledKeys;
+    private int totalScore, rollsLeft, dieCount;
+    private Button rollDie, keepDie, resetGame, leaveGame;
+    private Die currentDie;
+
+    private TextView rollsLeftText, scoreText;
+    private MediaPlayer mp;
+
+    public static int randomDiceValue(){
+        return RANDOM.nextInt(6) + 1;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event400_metres);
+
+        rollDie = (Button) findViewById(R.id.button_roll_400M);
+        keepDie = (Button) findViewById(R.id.button_keep_400M);
+        keepDie.setEnabled(false);
+        resetGame = (Button) findViewById(R.id.button_replay_400M);
+        resetGame.setEnabled(false);
+        leaveGame = (Button) findViewById(R.id.button_done_400M);
+
+        rollsLeftText = (TextView) findViewById(R.id.tv400MRollsLeft);
+        scoreText = (TextView) findViewById(R.id.tv400MScore);
+
+        totalScore = 0;
+        rollsLeft = 9;
+        dieCount = 1;
+
+        ivRolledDice = new ArrayList<ImageView>();
+        rolledKeys = new ArrayList<String>();
+
+        rolledKeys.add("rollDie1");
+        rolledKeys.add("rollDie2");
+        rolledKeys.add("rollDie3");
+        rolledKeys.add("rollDie4");
+        rolledKeys.add("rollDie5");
+        rolledKeys.add("rollDie6");
+        rolledKeys.add("rollDie7");
+        rolledKeys.add("rollDie8");
+
+        ivRolledDice.add(findViewById(R.id.iv400MRollDie1));
+        ivRolledDice.add(findViewById(R.id.iv400MRollDie2));
+        ivRolledDice.add(findViewById(R.id.iv400MRollDie3));
+        ivRolledDice.add(findViewById(R.id.iv400MRollDie4));
+        ivRolledDice.add(findViewById(R.id.iv400MRollDie5));
+        ivRolledDice.add(findViewById(R.id.iv400MRollDie6));
+        ivRolledDice.add(findViewById(R.id.iv400MRollDie7));
+        ivRolledDice.add(findViewById(R.id.iv400MRollDie8));
+
+        rolledDice = new Dice(ivRolledDice, rolledKeys);
+        rolledDice.getDiceList().get("rollDie1").makeVisible();
+        rolledDice.getDiceList().get("rollDie2").makeVisible();
+
+        rollDie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Animation anim1 = AnimationUtils.loadAnimation(Event400Metres.this, R.anim.shake);
+                final Animation anim2 = AnimationUtils.loadAnimation(Event400Metres.this, R.anim.shake);
+                rollsLeft--;
+                rollsLeftText.setText(Integer.toString(rollsLeft));
+                keepDie.setEnabled(true);
+
+                final Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        int value = randomDiceValue();
+                        int res = 0;
+                        switch (value) {
+                            case 1:
+                                res = R.drawable.die1;
+                                break;
+                            case 2:
+                                res = R.drawable.die2;
+                                break;
+                            case 3:
+                                res = R.drawable.die3;
+                                break;
+                            case 4:
+                                res = R.drawable.die4;
+                                break;
+                            case 5:
+                                res = R.drawable.die5;
+                                break;
+                            case 6:
+                                res = R.drawable.die6;
+                                break;
+                        }
+
+                        if (animation == anim1) {
+                            rolledDice.getDiceList().get(rolledKeys.get(dieCount-1)).getDieFaceView().setImageResource(res);
+                            rolledDice.getDiceList().get(rolledKeys.get(dieCount-1)).setValue(value);
+                        } else {
+                            rolledDice.getDiceList().get(rolledKeys.get(dieCount)).getDieFaceView().setImageResource(res);
+                            rolledDice.getDiceList().get(rolledKeys.get(dieCount)).setValue(value);
+                        }
+
+
+
+                        if (rolledDice.getDiceList().get(rolledKeys.get(dieCount-1)).getValue() == 6){
+                            rolledDice.getDiceList().get(rolledKeys.get(dieCount-1)).getDieFaceView().setBackgroundColor(Color.BLACK);
+                        } else {
+                            rolledDice.getDiceList().get(rolledKeys.get(dieCount-1)).getDieFaceView().setBackgroundColor(Color.GREEN);
+                        }
+                        if (rolledDice.getDiceList().get(rolledKeys.get(dieCount)).getValue() == 6){
+                            rolledDice.getDiceList().get(rolledKeys.get(dieCount)).getDieFaceView().setBackgroundColor(Color.BLACK);
+                        } else {
+                            rolledDice.getDiceList().get(rolledKeys.get(dieCount)).getDieFaceView().setBackgroundColor(Color.GREEN);
+                        }
+
+                        if (rollsLeft == ((7 - dieCount) / 2) || (dieCount == 8 && rollsLeft == 0)){
+                            rollDie.setEnabled(false);
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                };
+                anim1.setAnimationListener(animationListener);
+                anim2.setAnimationListener(animationListener);
+
+                rolledDice.getDiceList().get(rolledKeys.get(dieCount-1)).getDieFaceView().startAnimation(anim1);
+                rolledDice.getDiceList().get(rolledKeys.get(dieCount)).getDieFaceView().startAnimation(anim2);
+            }
+        });
+
+        keepDie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keepDie.setEnabled(false);
+                int dieValue1 = rolledDice.getDiceList().get(rolledKeys.get(dieCount-1)).getValue();
+                if (dieValue1 == 6){
+                    totalScore -= dieValue1;
+                } else {
+                    totalScore += dieValue1;
+                }
+
+                int dieValue2 = rolledDice.getDiceList().get(rolledKeys.get(dieCount)).getValue();
+                if (dieValue2 == 6){
+                    totalScore -= dieValue2;
+                } else {
+                    totalScore += dieValue2;
+                }
+
+                scoreText.setText(Integer.toString(totalScore));
+                if (dieCount < 7){
+                    rolledDice.getDiceList().get(rolledKeys.get(dieCount + 1)).makeVisible();
+                    rolledDice.getDiceList().get(rolledKeys.get(dieCount + 2)).makeVisible();
+                }
+
+                if (dieCount < 7){
+                    rollDie.setEnabled(true);
+                } else {
+                    rollDie.setEnabled(false);
+                    resetGame.setEnabled(true);
+                }
+
+                dieCount += 2;
+            }
+        });
+
+        resetGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                totalScore = 0;
+                rollsLeft = 9;
+                dieCount = 1;
+
+                scoreText.setText("-");
+                rollsLeftText.setText(Integer.toString(rollsLeft));
+
+                rolledDice.MakeOnes();
+                rolledDice.MakeHidden();
+                rolledDice.ChangeBackgroundColor(Color.WHITE);
+
+                rolledDice.getDiceList().get("rollDie1").makeVisible();
+                rolledDice.getDiceList().get("rollDie2").makeVisible();
+
+                rollDie.setEnabled(true);
+                keepDie.setEnabled(false);
+                resetGame.setEnabled(false);
+            }
+        });
+
+        leaveGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
